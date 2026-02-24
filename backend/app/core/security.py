814287b80c7d4e05a02de84159617be2917ef188
moodykhalif23 <b.wayatg7@gmail.com>
2026-02-24 11:45:46 +0300

    gateTime not a function error fixed

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -6,9 +6,12 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 
 from app.core.config import settings
-
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto",
+    bcrypt__default_rounds=12,
+    bcrypt__ident="2b"
+)
 
 
 TokenType = Literal["access", "refresh"]
