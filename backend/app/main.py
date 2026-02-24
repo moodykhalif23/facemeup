d9009a61919f3b56b667d8ac92326073479925a8
moodykhalif23 <b.wayatg7@gmail.com>
2026-02-24 11:43:01 +0300

    gateTime not a function error fixed

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,7 +36,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:8081", 
         "http://127.0.0.1:8081", 
-        "http://localhost:19006"
+        "http://localhost:19006",
     ],
     allow_credentials=True,
     allow_methods=["*"],
