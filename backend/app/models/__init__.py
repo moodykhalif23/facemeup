0bb6a87d1736eebfe9a4bbdfa4eb7d1aad2bbc76
moodykhalif23 <b.wayatg7@gmail.com>
2026-04-27 15:47:12 +0300

    hr behaviour in ai anomalies query fixed

--- backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.order import LoyaltyLedger, Order
-from app.models.product import ProductCatalog
-from app.models.profile import SkinProfileHistory
-from app.models.user import RefreshToken, User
-
-__all__ = ["User", "RefreshToken", "SkinProfileHistory", "Order", "LoyaltyLedger", "ProductCatalog"]
